64554a6e22052995e6b9796ebd5df773f68e90b5
msritambhra <vritambhara@gmail.com>
2021-07-29 19:10:27 +0530

    routing in navbar

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import './App.css';
+import Header from './components/Header';
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const World = () => (
+  <div>
+    <h2>World</h2>
+  </div>
+);
+
+const Politics = () => (
+  <div>
+    <h2>Politics</h2>
+  </div>
+);
+
+const Technology = () => (
+  <div>
+    <h2>Technology</h2>
+  </div>
+);
+
+function App() {
+  return (
+    <div className='App'>
+      <Router>
+        <Route path='/:page' component={Header} />
+        <Route exact path='/' component={Header} />
+
+        <Route exact path='/' component={Home} />
+        <Route exact path='/home' component={Home} />
+        <Route exact path='/world' component={World} />
+        <Route exact path='/politics' component={Politics} />
+        <Route exact path='/technology' component={Technology} />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
